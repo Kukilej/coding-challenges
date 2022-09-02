1ac1cbdf9d91d1a6bc0cf7cba6c367453ef34384
Marko Kuzminac <marko19933@live.com>
2022-09-02 10:29:58 +0200

    Two pointers 2sum2 & 3sum

--- src/main/java/TwoPointers.java
+++ src/main/java/TwoPointers.java
@@ -1,9 +1,8 @@
-import java.util.List;
+import java.util.*;
 
 public class TwoPointers {
 
     //https://leetcode.com/problems/valid-palindrome/
-    //after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters
     public static boolean isPalindrome(String s) {
 
         char[] convertedString = s.toLowerCase().toCharArray();
@@ -24,26 +23,38 @@ public class TwoPointers {
     }
 
 
-
-
-
-
-    public int[] twoSum(int[] numbers, int target) {
-//    int start = 0;
-//    int end = numbers.length - 1;
-//        while (start < end) {
-//        int sum = numbers[start] + numbers[end];
-//        if (sum == target) return new int[]{start + 1, end + 1};
-//        else if (sum >= target) {
-//            end--;
-//        } else start++;
-//    }
-//        return null;
-        return null;
+    //https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+    public static int[] twoSum(int[] numbers, int target) {
+        int start = 0;
+        int end = numbers.length - 1;
+        while (start < end) {
+            int sum = numbers[start] + numbers[end];
+            if (sum == target) return new int[]{start + 1, end + 1};
+            else if (sum > target) {
+                end--;
+            } else start++;
+        }
+        return new int[]{};
     }
 
-    public List<List<Integer>> threeSum(int[] nums) {
-        return null;
+    //https://leetcode.com/problems/3sum/
+    public static List<List<Integer>> threeSum(int[] nums) {
+        if (nums.length < 3) return Collections.emptyList();
+        Set<List<Integer>> distinctPairs = new HashSet<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int start = i + 1;
+            int end = nums.length - 1;
+            while (start < end) {
+                int sum = nums[i] + nums[start] + nums[end];
+                if (sum == 0) {
+                    distinctPairs.add(List.of(nums[i], nums[start++], nums[end--]));
+                } else if (sum > 0) end--;
+                else start++;
+            }
+        }
+        return distinctPairs.stream().toList();
     }
 
 }
+
--- src/test/java/TwoPointersTest.java
+++ src/test/java/TwoPointersTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TwoPointersTest {
@@ -7,6 +10,15 @@ public class TwoPointersTest {
     @Test
     public void testIsPalindrome() {
         assertTrue( TwoPointers.isPalindrome("A man, a plan, a canal: Panama"));
+    }
+
+    @Test
+    public void testTwoSumSortedArray() {
+        assertArrayEquals(new int[]{1,2}, TwoPointers.twoSum(new int[]{2,7,11,15}, 9));
+    }
+    @Test
+    public void testThreeSum() {
+        assertArrayEquals(Arrays.asList(Arrays.asList(-1,-1,2),Arrays.asList(-1,0,1)).toArray(), TwoPointers.threeSum(new int[]{-1,0,1,2,-1,-4}).toArray());
     }
 
 }
